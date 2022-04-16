6ff051d69e6b291ad898027d5fa88a0bdff7f8b4
Arik Alexandrov <arikxl@gmail.com>
2022-04-16 23:58:36 +0300

    setup rouyer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+
+
 import './App.css';
+import Home from "./pages/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LOOLA
-        </a>
-      </header>
-    </div>
+    <Routes>
+      LOOLA
+      <Route path="/" element={<Home />}/>
+      {/* <Route path="/chats" element={<Home />}/> */}
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
