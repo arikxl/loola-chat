130057eff681c36f8f4d73784a31e5017da9bd89
Arik Alexandrov <arikxl@gmail.com>
2022-04-29 23:26:07 +0300

    context

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
+import ChatProvider from './context/chatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChatProvider>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
+    </ChatProvider>
   </React.StrictMode>
 );
 
